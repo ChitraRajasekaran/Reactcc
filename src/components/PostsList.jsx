09ecdc6dbd20a81ee890673b2b64072bb33d1dc0
Chitra Rajasekaran <crinspire01@gmail.com>
2024-01-08 22:04:39 -0500

    Lifting the state up

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -1,13 +1,24 @@
+import { useState } from 'react';
+
 import Post from './Post'
 import NewPost from './NewPost'
 import classes from './PostsList.module.css'
 
 function PostsList(props) {
+    const [enteredBody,setEnteredBody] = useState('')
+    const [enteredAuthor,setEnteredAuthor] = useState('');
+
+    function bodyChangeHandler(event) {
+        setEnteredBody(event.target.value);
+    }
+    function authorChangeHandler(event) {
+        setEnteredAuthor(event.target.value);
+    }
     return (
     <>
-        <NewPost />
+        <NewPost onBodyChange = {bodyChangeHandler} onAuthorChange = {authorChangeHandler}/>
         <ul className={classes.posts}>
-        <Post author= 'Chitra' body='hello'/>
+        <Post author= {enteredAuthor} body={enteredBody}/>
         <Post author='Raj' body='Bonjour'/>
         </ul>
     </>
