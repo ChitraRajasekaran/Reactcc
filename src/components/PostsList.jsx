d935a417ed0d0dcc67d3d033a1f0cc8f7f68792b
Chitra Rajasekaran <crinspire01@gmail.com>
2024-01-09 21:26:26 -0500

    add childrem prop and conditional content

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -2,11 +2,17 @@ import { useState } from 'react';
 
 import Post from './Post'
 import NewPost from './NewPost'
+import Modal from './Modal';
 import classes from './PostsList.module.css'
 
 function PostsList(props) {
-    const [enteredBody,setEnteredBody] = useState('')
+    const[modalIsVisible,setModalIsVisible] = useState(true);
+    const [enteredBody,setEnteredBody] = useState('');
     const [enteredAuthor,setEnteredAuthor] = useState('');
+
+    function hideModalHandler() {
+        setModalIsVisible(false);
+    }
 
     function bodyChangeHandler(event) {
         setEnteredBody(event.target.value);
@@ -16,13 +22,19 @@ function PostsList(props) {
     }
     return (
     <>
-        <NewPost onBodyChange = {bodyChangeHandler} onAuthorChange = {authorChangeHandler}/>
+        {modalIsVisible ? (
+        <Modal onClose={hideModalHandler}>
+            <NewPost 
+            onBodyChange = {bodyChangeHandler} 
+            onAuthorChange = {authorChangeHandler}/>
+        </Modal>
+        ) : false}
         <ul className={classes.posts}>
         <Post author= {enteredAuthor} body={enteredBody}/>
         <Post author='Raj' body='Bonjour'/>
         </ul>
     </>
-    );
+    ) ;
 }
 
 
