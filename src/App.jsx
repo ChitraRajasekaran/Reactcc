d3bcf906b1380922bbe42d61a6af00311249d99b
Chitra Rajasekaran <crinspire01@gmail.com>
2024-01-10 23:10:22 -0500

    Adding a shared header

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
+import { useState } from "react";
 import PostsList from "./components/PostsList";
+import MainHeader from "./components/MainHeader";
 
 function App() {
-  return ( <main>
-
-    <PostsList />
-    </main>
+  const[modalIsVisible,setModalIsVisible] = useState(true);
+  function showModalHandler() {
+    setModalIsVisible(true);
+  }
+  function hideModalHandler() {
+    setModalIsVisible(false);
+  }
+  return ( 
+    <>
+      <MainHeader />
+      <main>
+        <PostsList isPosting={modalIsVisible} />
+      </main>
+    </>
   );
 }
 
